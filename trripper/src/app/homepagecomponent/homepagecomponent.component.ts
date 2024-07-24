aa1c789b65d20e93e4d6f95c94d51b1eb4eaa9ad
Aryan Kumar Singh <aryankrsingh7@gmail.com>
2024-07-24 16:28:24 +0530

    Created application

---
+++ trripper/src/app/homepagecomponent/homepagecomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepagecomponent',
+  templateUrl: './homepagecomponent.component.html',
+  styleUrl: './homepagecomponent.component.css'
+})
+export class HomepagecomponentComponent {
+
+}
